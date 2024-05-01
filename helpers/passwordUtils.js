cecdc555c0ea75cdc5bdf55814533d5f2cc474f1
Jayaram <akulajayaram96@gmail.com>
2024-05-01 13:40:46 +0530

    Update passwordUtils.js

--- helpers/passwordUtils.js
+++ helpers/passwordUtils.js
@@ -1,5 +1,18 @@
+// passwordUtils.js
+
 const bcrypt = require('bcrypt');
 
-// Password utility functions here
+// Function to hash a password
+async function hashPassword(password) {
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+  return hashedPassword;
+}
 
-module.exports = { /* exported functions */ };
+// Function to compare a password with its hash
+async function comparePassword(password, hashedPassword) {
+  const isMatch = await bcrypt.compare(password, hashedPassword);
+  return isMatch;
+}
+
+module.exports = { hashPassword, comparePassword };
